1e52660d69b44147735df52906599f25a5c9b008
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2024-11-01 20:02:09 +0530

    made server run

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
-const express = require("express");
-const app = express();
-const port = 5000;
+const express = require("express")
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// root route
+app.get("/", (req,res) => {res.send("hello world this is zahabiya running port on 5001")})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// port 5001 running the server
+const port = 5001 // add env after env is made 
+app.listen(port, () => {console.log(`Server is running on port ${port}`)})
+
+
+
+
+// this is what sujit wrote 
+// const express = require("express");
+// const app = express();
+
+// // root route
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+
+// // port 5001 running the server
+// const port = 5001;
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
