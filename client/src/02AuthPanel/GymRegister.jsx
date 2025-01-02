239201b9f294ef7065379f36b7e96c0f448b5890
Zahabiya Tawawala <zahabiyatawawala1@gmail.com>
2025-01-02 21:23:18 +0530

    daily commits

--- client/src/02AuthPanel/GymRegister.jsx
+++ client/src/02AuthPanel/GymRegister.jsx
@@ -4,7 +4,7 @@ const GymRegister = () => {
   return (
     <div>
       this is the gym resgistration site registration for gym owners
-      {/* this section need to develope next work on frontend */}
+      {/* this section need to develope next work on frontend and backend */}
     </div>
   )
 }
